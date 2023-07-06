8a8811d7300dc988c8d990e306f24a14cd56809a
GonzagaDevTest <>
2023-07-05 23:18:18 -0300

    add utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 
 const Utils = require('../lib/utils/Utils')
+const DateTimeUtils = require('../lib/utils/DateTimeUtils')
+const DocumentUtils = require('../lib/utils/DocumentUtils')
 const Commands = require('../lib/support')
 
 module.exports = {
     Utils,
+    DateTimeUtils,
+    DocumentUtils,
     Commands
 }
 
